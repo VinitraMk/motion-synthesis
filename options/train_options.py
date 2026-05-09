7f632b4d25879f2def8b949c5aa0d67c5d3c49cc
Vinitra Muralikrishnan <vinitramk@gmail.com>
2026-05-09 01:17:31 -0700

    filing main.ipynb notebook

--- options/train_options.py
+++ options/train_options.py
@@ -6,7 +6,7 @@ class TrainOptions():
         self.initialized = False
     
     def initialize(self):
-        every_e_interval = 20
+        every_e_interval = 5 
         self.parser.add_argument('--name', type = str, default = 'test', help = 'Name of the trial')
         self.parser.add_argument('--checkpoints_dir', type = str, default = './checkpoints', help = 'Models and other data are saved here')
         self.parser.add_argument('--experiment_dir', type = str, default = './exp_results/vq-vae-setup', help = 'Save experiment loss logs and reports')
@@ -23,17 +23,17 @@ class TrainOptions():
         self.parser.add_argument('--save_every_e', type=int, default=every_e_interval, help='Frequency of saving models (by epoch)')
         self.parser.add_argument('--eval_every_e', type=int, default=every_e_interval, help='Frequency of animation results (by epoch)')
         self.parser.add_argument('--is_continue', action="store_true", help='Is this trail continued from previous trail?')
-        self.parser.add_argument('--max_epoch', type=int, default=200, help='Training iterations')
+        self.parser.add_argument('--max_epoch', type=int, default=10, help='Training iterations')
         self.parser.add_argument('--save_latest', type=int, default=every_e_interval, help='Frequency of saving models (by iteration)')
 
 
         self.initialized = True
 
-    def parse(self):
+    def parse(self, args = []):
         if not(self.initialized):
             self.initialize()
         
-        self.options = self.parser.parse_args()
+        self.options, _ = self.parser.parse_known_args(args = args)
         self.options.is_train = True
 
         return self.options
